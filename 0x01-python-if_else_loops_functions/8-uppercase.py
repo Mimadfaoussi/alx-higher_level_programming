0ad440891c0443d895ce74b3f61121d9b512b59a
imad faoussi <imadfaoussi@gmail.com>
2023-12-13 21:54:07 -0800

    to upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new = ''
+    for char in str:
+        if (ord(char) >= 97 and ord(char) <= 122):
+            new = new + chr(ord(char) - 32)
+        else:
+            new = new + char
+    return (new)
