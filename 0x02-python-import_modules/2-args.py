e6a715ee8048a7516f94ee3c20c746db72486671
imad faoussi <imadfaoussi@gmail.com>
2023-12-15 01:16:43 -0800

    fixing style

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         print("{}: {}".format(length - 1, sys.argv[1]))
     elif (length > 2):
         print("{} arguments:".format(length - 1))
-        for i in range (1, length):
+        for i in range(1, length):
             print("{}: {}".format(i, sys.argv[i]))
