f33b9a255d08bfcce92a6d875b0fe5bf082d028f
imad faoussi <imadfaoussi@gmail.com>
2023-12-16 05:27:11 -0800

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if (matrix != [[]]):
+    if (matrix == [[]]):
+        print()
+    else:
         for line in matrix:
             for elm in line:
-                print("{:d}".format(elm), end='')
+                print("{:d}".format(elm), end=' ')
             print("{}".format())
-    else:
-        print("{}".format())
