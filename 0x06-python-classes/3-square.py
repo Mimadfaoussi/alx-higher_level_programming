598347c9bdf27fdbce22f2c5f9919c74b65931b5
imad faoussi <imadfaoussi@gmail.com>
2023-12-24 05:28:10 -0800

    adding public instance methode to the square class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" this module we will Write a class Square that defines a square by :
+    Private instance attribute: size
+    Instantiation with optional size: def __init__(self, size=0) """
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        if size not in isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
