ccf2c9d3a8a13118d76b5925d116e4df7611b3b7
mfaoussi <imadfaoussi@gmail.com>
2024-03-16 01:10:09 +0100

    copiying the file

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def add(a, b):
-<<<<<<< HEAD
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
     return (a + b)
-=======
-    return (a + b)
->>>>>>> a07c859d5c0ec5282d7479a3acc41e21daaa5f3d
