48883c80e2e8d642dcd0a64a0cd316f554dd5b5b
imadfaoussi <imadfaoussi@gmail.com>
2024-03-16 14:29:06 -0700

    print alphabet uppercase

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if (__name__ == "__main__"):
+    from calculator_1 import add, div, mul, sub
+    from sys import argv
+    if (len(argv) != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        if (argv[2] == '+'):
+            print("{} {} {} = {}".format)
+        elif (argv[2] == '+'):
+            print
+        elif (argv[2] == '+'):
+            print
+        elif (argv[2] == '+'):
+            eqeq
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
