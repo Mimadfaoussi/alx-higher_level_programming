fac507cbc8fd17f15c195e4a770844e33d0f3892
Mimadfaoussi <imadfaoussi@gmail.com>
2024-04-17 00:49:47 +0200

    from json to data python

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""  From JSON string to Object """
+
+
+def from_json_string(my_str):
+    """ we're converting JSON string to python data structure """
+    return (json.loads(json_string))
