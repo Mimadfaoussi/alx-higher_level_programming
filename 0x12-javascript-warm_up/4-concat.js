bdcf25cc155c446f079e63dc9c117660e7e9aa97
mfaoussi <imadfaoussi@gmail.com>
2024-06-18 17:03:57 +0200

    4. Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// prints two arguments passed to it, in the following format: “ is ”
+
+if (process.argv[2])
+	console.log(process.argv[2] + " is ");
+else
+	console.log("undefined is ");
+if (process.argv[3])
+	console.log(argv[3]);
+else
+	console.log("undefined");
