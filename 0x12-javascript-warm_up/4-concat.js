feb42e302292b4b3f126f8b2556d08dda5d355c4
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-19 03:00:39 +0200

    fix semistandard

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-if (process.argv[2])
-	console.log(process.argv[2] + ' is ');
-else
-	console.log('undefined is ');
-if (process.argv[3])
-	console.log(argv[3]);
-else
-	console.log('undefined');
+if (process.argv[2]) { console.log(process.argv[2] + ' is '); } else { console.log('undefined is '); }
+if (process.argv[3]) { console.log(process.argv[3]); } else { console.log('undefined'); }
