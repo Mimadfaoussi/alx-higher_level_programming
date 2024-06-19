a0049534f7108926f993eb076ccc25c5422c6910
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-19 03:52:01 +0200

    c is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (!process.argv[2] || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+}
+if (parseInt(process.argv[2]) > 0) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
+}
