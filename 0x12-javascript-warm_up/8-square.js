34d7fec0b2a54402f0a0d158e017f315855ae030
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-19 04:03:20 +0200

    fix 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 if (!process.argv[2] || isNaN(process.argv[2])) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else if (parseInt(process.argv[2]) > 0) {
-    for (let i = 0; i < parseInt(process.argv[2]); i++) {
-      let x = '';
-      for (let j = 0; j < parseInt(process.argv[2]); j++){
-        x = x + 'x';
-      }
-      console.log(x);
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    let x = '';
+    for (let j = 0; j < parseInt(process.argv[2]); j++) {
+      x = x + 'x';
     }
+    console.log(x);
+  }
 }
