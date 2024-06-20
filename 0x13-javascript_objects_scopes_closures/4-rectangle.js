5aef0c32ca15645dbb939bf4bc22d0344472ea5d
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-20 19:45:53 +0200

    adding double and rotate

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = h;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let word;
+    for (let i = 0; i < this.height; i++) {
+      word = '';
+      for (let j = 0; j < this.width; j++) {
+        word = word + 'X';
+      }
+      console.log(word);
+    }
+  }
+
+  rotate () {
+    const w = this.width;
+    this.width = this.height;
+    this.height = w;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
