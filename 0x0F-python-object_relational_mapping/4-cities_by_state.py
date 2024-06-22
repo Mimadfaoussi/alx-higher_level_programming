7b0d9dee8d8fe3b800c4990887867a779bd6abf2
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-22 06:18:16 +0200

    joining the tables and showing the proper data

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
         db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3], port=3306)
         cursor = db.cursor()
-        query = "SELECT * FROM cities ORDER BY cities.id"
+        query = """SELECT cities.id, cities.name, states.name FROM cities
+                 INNER JOIN states ON states.id=cities.state_id"""
         cursor.execute(query)
         results = cursor.fetchall()
         for row in results:
