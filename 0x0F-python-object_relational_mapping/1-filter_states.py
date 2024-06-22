6375c487f020246d262958ff8d5f6d01ddcbe52e
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-22 04:05:58 +0200

    select a pattern at start and order by id

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" lists all states with a name starting with N (upper N) """
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 4):
+        print('wrong nb of args')
+        sys.exit(1)
+    else:
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        db_name = sys.argv[3]
+        db = MySQLdb.connect(host='localhost', user=mysql_username,
+                             passwd=mysql_password, db=db_name, port=3306)
+        cursor = db.cursor()
+        cursor.execute(""" SELECT * FROM states WHERE
+                        name LIKE 'N%' ORDER BY states.id""")
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+        cursor.close()
+        db.close()
