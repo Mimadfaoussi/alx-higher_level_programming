5f435030879c0e93095bcb20726353c134fcf59e
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-23 05:02:29 +0200

    getting the first object of the database

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+""" prints the first object from the database """
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 4):
+        print('wrong nb of args')
+        sys.exit(1)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_one = session.query(State).order_by(State.id).first()
+    if (state_one):
+        print("{}: {}".format(state_one.id, state_one.name))
+    else:
+        print('Nothing')
+    session.close()
