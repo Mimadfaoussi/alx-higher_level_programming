b65762792121441954f8a9b3caa41604086635a0
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-23 03:18:17 +0200

    fixing pycodestyle

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,10 @@ if __name__ == '__main__':
     if (len(sys.argv) != 4):
         print('wrong nb of args')
         sys.exit(1)
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
