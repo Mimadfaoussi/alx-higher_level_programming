13c9fd69dfd8892d95f814130115d18e393e638e
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-26 01:00:28 +0200

    square with a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a Square class which defines some attributes """
+
+
+class Square:
+    """ Class Square that has a private attr size """
+    def __init__(self, size):
+        """ the constructor of the square class"""
+        self.__size = size
