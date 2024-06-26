ca14a84d736816d9813594b6448ba8fdf5522b73
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-26 05:00:56 +0200

    print in a specific positon

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,8 +35,13 @@ class Square:
     @position.setter
     def position(self, value):
         """ setter of the position of square class """
-        if not isinstance(value, (int, int)):
+        if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(num, int) for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise ValueError("position must be a tuple of 2 positive integers")
+
         self.__position = value
 
     def area(self):
