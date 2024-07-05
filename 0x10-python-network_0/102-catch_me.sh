7575aca27fd4082e7cb7fdbc58e9acc90614b3fb
Mimadfaoussi <imadfaoussi@gmail.com>
2024-07-05 05:33:03 +0200

    you got me :)

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # making a request to the server and making it respond with you got me!
-curl -s -o /dev/null -w "You got me!" "$1"
+curl -s -o /dev/null -w "You got me!" 0.0.0.0:5000/catch_me
