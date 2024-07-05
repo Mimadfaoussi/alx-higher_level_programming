7967bc237a120064b410c4b4111d330638ffaec7
imad faoussi <imadfaoussi@gmail.com>
2024-07-05 01:51:17 -0700

    fetching an url using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# python script that fetches a url using urllib
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+    print(" - type: {}".format(type(html)))
+    print(" - content: {}".format(html))
+    print(" - utf8 content: {}".format(html.decode('utf-8')))
