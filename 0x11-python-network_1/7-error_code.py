64193dc965b024f2b2157d1364f73552939105c5
imad faoussi <imadfaoussi@gmail.com>
2024-07-06 01:17:27 -0700

    error code 1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" send a request to the url and display body
+response or error if its >= 400 """
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.get(url)
+    if(r.status_code >= 400):
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
